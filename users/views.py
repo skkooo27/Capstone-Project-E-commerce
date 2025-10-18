e619a02401a248f012ed13d453c4ce7fff808ec1
skkooo27 <sharlinekerubo27@gmail.com>
2025-10-18 21:08:46 +0300

    Added permission_classes to register view and imported AllowAny permission.

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets, status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView
 from django.contrib.auth.models import User
@@ -10,6 +11,7 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
 
 @api_view(['POST'])
+@permission_classes([AllowAny])
 def register(request):
     serializer = RegisterSerializer(data=request.data)
     if serializer.is_valid():
